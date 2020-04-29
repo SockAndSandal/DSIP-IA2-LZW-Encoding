6a527041b2ffa3891575bf437bc8b2ef95133288
KafkaOnTheInternet <arjun_shanewarne@yahoo.com>
2020-04-29 18:35:35 +0530

    second commit

--- test.py
+++ test.py
@@ -1,6 +1,30 @@
-import io
+fp = open('testtxt.txt')
 
-f = open('testtxt.txt')
-a = f.read(2)
-b = f.read(4)
-print(a, b)
+def compress(fp):
+    dictionary = {chr(i): i for i in range(256)}
+    contents = fp.read()
+    index = 256
+    prefix = ''
+    result = []
+    
+    for B in contents:
+        if prefix + B in dictionary:
+            prefix += B
+        else:
+            dictionary[prefix+B] = index
+            result.append(index)
+            index += 1
+            prefix = B
+    result.append(index)
+    for a in dictionary:
+        print(a)
+    return result, dictionary
+
+def decompress(dictionary, result):
+    output = ''
+    for B in result:
+        output += dictionary[result]
+    return output
+
+D = compress(fp)
+
